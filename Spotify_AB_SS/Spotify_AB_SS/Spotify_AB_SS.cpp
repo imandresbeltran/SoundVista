d80291920525efcbc8bc1d3dcdb8979351892c7a
Ssergio0 <sergio7salguero@gmail.com>
2023-10-01 18:39:03 -0600

    Implementación de nuevas acciones a la interfaz

--- Spotify_AB_SS/Spotify_AB_SS/Spotify_AB_SS.cpp
+++ Spotify_AB_SS/Spotify_AB_SS/Spotify_AB_SS.cpp
@@ -1,10 +1,53 @@
-
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
 #include <fstream>
 #include <sstream>
+#include <windows.h>
+#include <limits>
 #include "CDManager.h"
+
+void reproductor_Canciones() {
+	menu2:
+	int opcion;
+	std::cout << "\t1. Agregar Cancion." << std::endl;
+	std::cout << "\t2. Ver Cola de Reproduccion" << std::endl;
+	std::cout << "\t3. Ordenar" << std::endl;
+	std::cout << "\t4. Reproduccion Actual" << std::endl;
+	std::cout << "\t5. Reproducir Siguiente" << std::endl;
+	std::cout << "\t6. Volver al menu principal" << std::endl;
+	std::cout << "Elige una opcion:" << std::endl; std::cin >> opcion;
+	switch (opcion)
+	{
+	case 1:std::cout << "Haz seleccionado agregar Canción." << std::endl;
+		// Pausar y esperar que el usuario presione enter para continuar
+		/*std::cout << "\nPresione ENTER para continuar...";
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin.get();*/
+		system("cls");
+		break;
+	case 2:std::cout << "Haz seleccionado Ver Cola de Reproduccion." << std::endl;
+		system("cls");
+		break;
+	case 3:std::cout << "Ordenar." << std::endl;
+		system("cls");
+		break;
+	case 4:std::cout << "Haz seleccionado Reproduccion Actual." << std::endl;
+		system("cls");
+		break;
+	case 5:std::cout << "Haz seleccionado Reproducir Siguiente." << std::endl;
+		system("cls");
+		break;
+	case 6:std::cout << "Haz seleccionado Volver al menu principal." << std::endl;
+		system("cls");
+		return;
+	default:std::cout << "Opcion no valida, vuelte a intentar." << std::endl;
+		std::cin.get();
+		Sleep(1500);
+		system("cls");
+		goto menu2;		
+	}
+}
 
 int main()
 {
@@ -14,22 +57,40 @@ int main()
 	std::cout << "-----------------------------------SPOTIFY-------------------------------------\nPorfavor Seleccione una opcion"<< std::endl;
 	std::cout << "\t1. Cargar respaldo de archivos" << std::endl;
 	std::cout << "\t2. Reproducir Canciones" << std::endl;
+	std::cout << "\t3. Salir del programa" << std::endl;
 	std::cout << "SELECCIONE UNA OPCION:" << std::endl; 
-	std::cin >> opcion;
+	
+	while (!(std::cin >> opcion)) {
+		/*std::cout << "Por favor, ingrese un número válido: ";
+		std::cin.clear(); // Limpia el estado de error de cin
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignora caracteres inválidos en el búfer*/
+	}
 
 	switch (opcion)
 	{
 	case 1: std::cout<<"Haz seleccionado Cargar respaldo de archivos\n";
 		break;
+
 	case 2: std::cout << "Haz seleccionado Reproducir Canciones\n";
-		break;
-	default:std::cout << "Opcion no valida, vuelte a intentar. Presione cualquier tecla para continuar" << std::endl;
+		Sleep(2000);
+		system("cls");//esto Limpiara la consola
+		reproductor_Canciones();
+		
+		goto menu;
+		
+
+	case 3: std::cout<<"Haz elegido salir del programa, adios." << std::endl;
+		return 0;
+
+	default:std::cout << "Opcion no valida, vuelte a intentar." << std::endl;
 		std::cin.get();
-		system("cls");//esto Limpiar� la consola
+		Sleep(1500);
+		system("cls");//esto Limpiara la consola
 		goto menu;//Llevara devuelta al usuario al principio del menu
-		break;
+		
 	}
 #pragma endregion
+
 
 #pragma region agregarCD
 	CDManager collection;
@@ -41,6 +102,10 @@ int main()
 	collection.loadBackups(folderPath);
 
 	return 0;
+#pragma endregion
+
+	
     
+
 }
 
