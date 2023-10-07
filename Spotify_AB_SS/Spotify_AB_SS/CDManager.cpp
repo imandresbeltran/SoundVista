dfed00100bcda535483b5350f4ed9226d3abae1c
Andres Beltran <cabeltran@correo.url.edu.gt>
2023-10-07 14:36:57 -0600

    Creacion y funcionamiento de los métodos para cargar respaldos y para agregar canciones

--- Spotify_AB_SS/Spotify_AB_SS/CDManager.cpp
+++ Spotify_AB_SS/Spotify_AB_SS/CDManager.cpp
@@ -14,8 +14,7 @@ CDManager::CDManager() {}
 void CDManager::cargarRespaldos(const string& rutaCarpeta) {
 	listaCDs.clear();
 
-    if (!fs::exists(rutaCarpeta) || !fs::is_directory(rutaCarpeta
-    )) {
+    if (!fs::exists(rutaCarpeta) || !fs::is_directory(rutaCarpeta)) {
         cout << "La carpeta especificada no existe o no es una carpeta válida." << endl;
         return;
     }
@@ -25,14 +24,93 @@ void CDManager::cargarRespaldos(const string& rutaCarpeta) {
         if (entry.is_regular_file()) {
             const string archivo = entry.path().string();
 
-            if (fs::path(archivo).extension() == ".txt") {
-                ifstream archivo;
+            if (fs::path(archivo).extension() == ".txt") { //Verificar que el archivo tenga la extensión .txt
+                ifstream archivoCD(archivo);
+
+                if (archivoCD.is_open()) {
+                    string nombreCD = fs::path(archivo).stem().string(); //Obtener el nombre del archivo sin extensión
+                    CD nuevoCD(nombreCD);
+
+                    string linea;
+                    int lineasLeidas = 0;
+
+                    while (getline(archivoCD, linea)) {
+                        //Procesar cada línea del archivo
+                        istringstream ss(linea);
+                        string nombre, artista, duracion;
+
+                        if (getline(ss, nombre, '|') && getline(ss, artista, '|') && getline(ss, duracion, '|')) {
+                            //Verificar si la línea tiene el formato correcto
+
+                            Cancion cancion(nombre, artista, duracion);
+                            nuevoCD.addSong(cancion);
+                            lineasLeidas++;
+                        }
+                        else {
+                            cout << "Error en el archivo '" << archivo << "': Formato de línea incorrecto en la línea " << (lineasLeidas + 1) << endl;
+                        }
+                    }
+
+                    //Verificar si se leyeron canciones del CD
+                    if (lineasLeidas > 0) {
+                        listaCDs.push_back(nuevoCD);
+                        cout << "Se cargó el CD '" << nombreCD << "' con " << lineasLeidas << " canciones." << endl;
+                    }
+                    else {
+                        cout << "El archivo '" << archivo << "' no contiene canciones válidas." << endl;
+                    }
+
+                    archivoCD.close();
+                }
+                else {
+                    cout << "No se pudo abrir el archivo '" << archivo << "'." << endl;
+                }
             }
         }
     }
 }
 void CDManager::agregarCancion() {
-    // Implementar la función para agregar una canción a la cola de reproducción
+    //Verificar si hay CDs cargados
+    if (listaCDs.empty()) {
+        cout << "No hay CDs cargados. Cargue CDs ants de agregar canciones." << endl;
+        return;
+    }
+
+    //Mostrar la lista de CDs disponibles
+    cout << "Lista de CDs disponibles:" << endl;
+    for (size_t i = 0; i < listaCDs.size(); i++) cout << i + 1 << ". " << listaCDs[i].name << endl;
+
+    int seleccionCD;
+    cout << "Seleccione un CD (ingrese el número): ";
+    cin >> seleccionCD;
+
+    //Verificar si la selección de CD es válida
+    if (seleccionCD < 1 || seleccionCD > static_cast<int>(listaCDs.size())) {
+        cout << "Selección de CD no válida." << endl;
+        return;
+    }
+
+    //Mostrar la lista de canciones del CD seleccionado
+    const CD& cdSeleccionado = listaCDs[seleccionCD - 1];
+    cout << "Canciones del CD '" << cdSeleccionado.name << " :" << endl;
+    const vector<Cancion>& cancionesCD = cdSeleccionado.songs;
+    for(size_t i = 0; i < cancionesCD.size(); i++) cout << i + 1 << ". " << cancionesCD[i].name << " - " << cancionesCD[i].artist << " - " << cancionesCD[i].duration << endl;
+
+    int seleccionCancion;
+    cout << "Seleccione una canción (ingrese el número): ";
+    cin >> seleccionCancion;
+
+    //Verificar si la seleccion de la canción es válida
+    if (seleccionCancion < 1 || seleccionCancion > static_cast<int>(cancionesCD.size())) {
+        cout << "Selección de canción no válida." << endl;
+        return;
+    }
+
+    //Agregar la canción seleccionada a la cola de reproducción
+    const Cancion& cancionSeleccionada = cancionesCD[seleccionCancion - 1];
+    colaReproduccion.push_back(cancionSeleccionada);
+
+    cout << "Cancion '" << cancionSeleccionada.name << "' agregada a la cola de reproducción." << endl;
 }
 
 void CDManager::verColaReproduccion() {
